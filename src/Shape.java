4a79d85d5b33813c5b1b3f5c2271c2eda7b2b21a
peter <peter@peter-HP>
2014-11-20 08:14:24 -0500

    It's betterest.

---
+++ src/Shape.java
@@ -0,0 +1,6 @@
+abstract public class Shape {
+    abstract public String toString();
+    abstract public double area();
+    abstract public double perimitter();
+    int xPos, yPos, radius;
+}
---
+++ src/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle extends Shape{
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double area() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double perimitter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
