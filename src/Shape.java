997ba6b9b9eebc46c5b45c6f152bd45b1f507f7b
peter <peter@peter-HP>
2014-11-20 09:13:32 -0500

    Even bestest

--- src/Shape.java
+++ src/Shape.java
@@ -2,5 +2,5 @@ abstract public class Shape {
     abstract public String toString();
     abstract public double area();
     abstract public double perimitter();
-    int xPos, yPos, radius;
+    protected int xPos, yPos;
 }
